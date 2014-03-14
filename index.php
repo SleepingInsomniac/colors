9cff6613bf63a95312380a385cc88605d2d08cf0
Alex Clink <code@alexclink.com>
2014-03-14 00:50:36 -0400

    More descriptive

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 <head> 
 	<meta charset="utf-8"> 
 	<title>Colors</title> 
-	<meta name="description" content="HTML5 Docmument" />
-	<meta name="author" content="SitePoint" />
+	<meta name="description" content="Colors!" />
+	<meta name="author" content="Alex Clink" />
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8" />
